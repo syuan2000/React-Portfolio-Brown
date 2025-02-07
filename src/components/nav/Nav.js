85afe97f8649b7a593932fdc8156660957f75793
Wei Syuan, Li <wlcbn@mail.missouri.edu>
2025-02-06 21:45:34 -0800

    ignore

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -8,7 +8,7 @@ const Nav = () => {
 
   return (
     <nav>
-      <a href="#" className={active==='#'? 'active': ''} onClick={()=>setActive('#')}><AiOutlineHome /></a>
+      <a href="#" role="button" className={active==='#'? 'active': ''} onClick={()=>setActive('#')}><AiOutlineHome /></a>
       <a href="#about" className={active==='#about'? 'active': ''} onClick={()=>setActive('#about')}><AiOutlineUser /></a>
       <a href="#experience" className={active==='#experience'? 'active': ''} onClick={()=>setActive('#experience')}><MdWorkOutline /></a>
       <a href="#contact" className={active==='#contact'? 'active': ''} onClick={()=>setActive('#contact')}><AiOutlineMessage /></a>
