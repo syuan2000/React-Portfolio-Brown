e12fcee1a9bba4c392554cdb0c8af9af8213c464
Wei Syuan, Li <47014256+syuan2000@users.noreply.github.com>
2025-07-18 20:31:09 -0700

    Update Portfolio.js
    add sticky note

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import foodDiary from '../../assets/images/food_diary.jpg';
 import journal from '../../assets/images/journal.png';
-import jobSearch from '../../assets/images/job_search.png'
+import stickyNote from '../../assets/images/stickyNote.png'
 import './portfolio.css'
 
 const Portfolio = () => {
   const data = [
     {
       id: 1,
+      image: stickyNote,
+      title: "Sticky Note App",
+      github: "https://github.com/syuan2000/sticky-spark-notes-flow",
+      demo: "sticky-notes-theta-one.vercel.app"
+    },
+    {
+      id: 2,
       image: foodDiary,
       title: "Digital Food Photo Diary",
       github: "https://github.com/syuan2000/notion-diary",
       demo: "https://notion-diary-3b39d.web.app"
     },
     {
-      id: 2,
+      id: 3,
       image: journal,
       title: "Digital Calander Journal",
       github: "https://github.com/syuan2000/2020-Calendar.io",
       demo: "https://github.com/syuan2000/2020-Calendar.io"
     },
-    {
-      id: 3,
-      image: jobSearch,
-      title: "React Native Job Search",
-      github: "https://github.com/syuan2000/native",
-      demo: ""
-    },
+
   ]
   return (
     <section id="portforlio">
