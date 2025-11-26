52812809f4ce351e3106f11dcb0ad4791f037f46
Wei Syuan, Li <evali892000@gmail.com>
2025-11-25 22:34:25 -0800

    update styling

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,11 +1,41 @@
 import React from 'react'
-import aboutme from '../../assets/images/designer.png'
-import {TfiFolder} from 'react-icons/tfi';
-import {MdOutlineComputer} from 'react-icons/md';
-import {IoSchoolOutline} from 'react-icons/io5';
+import aboutVideo from '../../assets/images/about-video.webm'
+
+import { MdOutlineComputer } from 'react-icons/md';
+import { IoSchoolOutline } from 'react-icons/io5';
 import TrackVisibility from 'react-on-screen';
 import 'animate.css'
 import './about.css';
+
+
+
+const VideoPlayer = ({ isVisible }) => {
+  const videoRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (isVisible && videoRef.current) {
+      videoRef.current.currentTime = 0;
+      videoRef.current.play().catch(error => console.log("Autoplay prevented:", error));
+    } else if (!isVisible && videoRef.current) {
+      videoRef.current.pause();
+      videoRef.current.currentTime = 0;
+    }
+  }, [isVisible]);
+
+  return (
+    <video
+      ref={videoRef}
+      width="100%"
+      height="100%"
+      muted
+      playsInline
+      className='about__image'
+    >
+      <source src={aboutVideo} type="video/webm" />
+      Your browser does not support the video tag.
+    </video>
+  );
+};
 
 const About = () => {
   return (
@@ -16,39 +46,33 @@ const About = () => {
       <div className='container about__container'>
         <div className='about__me'>
           <div className='about__me-image'>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                  <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={aboutme} alt="About Image"/>
-              }
-              </TrackVisibility>
+            <TrackVisibility partialVisibility>
+              {({ isVisible }) => <VideoPlayer isVisible={isVisible} />}
+            </TrackVisibility>
           </div>
-          
+
         </div>
         <div className='about__content'>
-            <div className='about__cards'>
-              <article className='about__card'>
-                <IoSchoolOutline className='about__icon'/>
-                <h5>Education</h5>
-                <small>Bachelor of Information Technology </small>
-              </article>
-              <article className='about__card'>
-                <MdOutlineComputer className='about__icon'/>
-                <h5>Experience</h5>
-                <small>2 Year Software Engineer Working </small>
-              </article>
-              <article className='about__card'>
-                <TfiFolder className='about__icon'/>
-                <h5>Projects</h5>
-                <small>React Web Application </small>
-              </article>
-            </div>
-            <p>
-        Hi, I'm Eva Li! I'm a full-stack software engineer with experience building scalable systems end-to-end, but what I really enjoy most is shaping the client experience.
-  I love using React to turn ideas and designs into intuitive, user-centered products that feel smooth and enjoyable to use. <br />
-On the backend, I’ve worked with Python, .NET, and AWS to design APIs, streamline data flows, and optimize performance. <br />
-I’m always eager to grow and learn, and excited about products that combine creative thoughtful engineering with meaningful impact : )
-            </p>
-            <a href="#contact" className='btn btn-primary'>Let's Connect</a>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <IoSchoolOutline className='about__icon' />
+              <h5>Education</h5>
+              <small>Bachelor of Information Technology </small>
+            </article>
+            <article className='about__card'>
+              <MdOutlineComputer className='about__icon' />
+              <h5>Experience</h5>
+              <small>2 Years Software Engineer Working </small>
+            </article>
+
+          </div>
+          <p>
+            Hi, I'm Eva Li! I'm a full-stack software engineer with experience building scalable systems end-to-end, but what I really enjoy most is shaping the client experience.
+            I love using React to turn ideas and designs into intuitive, user-centered products that feel smooth and enjoyable to use. <br />
+            On the backend, I’ve worked with Python, .NET, and AWS to design APIs, streamline data flows, and optimize performance. <br />
+            I’m always eager to grow and learn, and excited about products that combine creative thoughtful engineering with meaningful impact : )
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Connect</a>
         </div>
       </div>
     </section>
--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,77 +1,100 @@
 import React from 'react';
 import './experience.css'
-import { BsPatchCheckFill } from 'react-icons/bs';
+import { SiTypescript, SiReact, SiNextdotjs, SiHtml5, SiCss3, SiPython, SiNodedotjs, SiMysql, SiCsharp, SiAmazonaws } from 'react-icons/si';
 
 const Experience = () => {
+  const frontendSkills = [
+    {
+      id: 1,
+      icon: <SiTypescript />,
+      name: "Typescript"
+    },
+    {
+      id: 2,
+      icon: <SiReact />,
+      name: "React.js"
+    },
+    {
+      id: 3,
+      icon: <SiNextdotjs />,
+      name: "Next.js"
+    },
+    {
+      id: 4,
+      icon: <SiHtml5 />,
+      name: "HTML"
+    },
+    {
+      id: 5,
+      icon: <SiCss3 />,
+      name: "CSS"
+    }
+  ];
+
+  const backendSkills = [
+    {
+      id: 1,
+      icon: <SiPython />,
+      name: "Python"
+    },
+    {
+      id: 2,
+      icon: <SiNodedotjs />,
+      name: "Node.js"
+    },
+    {
+      id: 3,
+      icon: <SiMysql />,
+      name: "SQL"
+    },
+    {
+      id: 4,
+      icon: <SiCsharp />,
+      name: "C#"
+    },
+    {
+      id: 5,
+      icon: <SiAmazonaws />,
+      name: "AWS",
+      description: "Lambda, S3, EC2, Load Balancer"
+    }
+  ];
+
   return (
     <section id="experience">
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
       <div className='container experience__container'>
-        <div class="experience__frontend">
+        <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
-
-            <article className="experience__details">
-              <BsPatchCheckFill className='experience__details_icon'/>
-              <div>
-                <h4>Javascript</h4>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className='experience__details_icon'/>
-              <div>
-                <h4>React.js</h4>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className='experience__details_icon'/>
-              <div>
-                <h4>HTML</h4>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className='experience__details_icon'/>
-              <div>
-                <h4>CSS</h4>
-              </div>
-            </article>
+            {frontendSkills.map(skill => (
+              <article key={skill.id} className="experience__details">
+                <div className="experience__icon">
+                  {skill.icon}
+                </div>
+                <div>
+                  <h4>{skill.name}</h4>
+                  {skill.description && <p>{skill.description}</p>}
+                </div>
+              </article>
+            ))}
           </div>
         </div>
-        <div class="experience__backend">
-        <h3>Backend & Cloud Development</h3>
+        <div className="experience__backend">
+          <h3>Backend & Cloud Development</h3>
           <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill className='experience__details_icon'/>
-              <div>
-                <h4>Python</h4>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className='experience__details_icon'/>
-              <div>
-                <h4>Node.js</h4>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className='experience__details_icon'/>
-              <div>
-                <h4>SQL</h4>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className='experience__details_icon'/>
-              <div>
-                <h4>C#</h4>
-              </div>
-            </article>
-            <article className="experience__details aws">
-              <BsPatchCheckFill className='experience__details_icon'/>
-              <div>
-                <h4>AWS</h4>
-                <p>Lambda, S3, EC2, Load Balancer</p>
-              </div>
-            </article>
+            {backendSkills.map(skill => (
+              <article key={skill.id} className={`experience__details${skill.description ? ' aws' : ''}`}>
+                <div className="experience__icon">
+                  {skill.icon}
+                </div>
+                <div>
+                  <h4>{skill.name}</h4>
+                  {skill.description && <p>{skill.description}</p>}
+                </div>
+              </article>
+            ))}
           </div>
         </div>
       </div>
